e7dd17dccf1f3ba3b59f5812c068584fcec4a194
TheLocust3 <jake.kinsella@gmail.com>
2018-02-05 17:45:58 -0500

    Add react redux boilerplate

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+
+import reducer from '../components/reducers/root-reducer';
 
 import Index from '../components/containers/Index';
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
 
 // Always start navigation at the top of the page
 const ScrollToTop = () => {
@@ -15,16 +22,19 @@ class Base extends React.Component {
 
     render() {
         return (
-            <div>
-                <Router>
-                    <div>
-                        <Route component={ScrollToTop} />
-                        <Switch>
-                            <Route exact path="/" component={Index} />
-                        </Switch>
-                    </div>
-                </Router>
-            </div>
+            <Provider store={store}>
+                <div>
+                    <Router>
+                        <div>
+                            <Route component={ScrollToTop} />
+
+                            <Switch>
+                                <Route exact path="/" component={Index} />
+                            </Switch>
+                        </div>
+                    </Router>
+                </div>
+            </Provider>
         );
     }
 }
