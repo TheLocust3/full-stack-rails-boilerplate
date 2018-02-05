6986ded5573f1c3cf94f1794354e4d7b917b4095
TheLocust3 <jake.kinsella@gmail.com>
2018-02-05 17:42:15 -0500

    Add react router setup

---
+++ app/javascript/components/containers/Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Index extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+                Hello World!
+            </div>
+        );
+    }
+}
+
+export default Index;
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Index from '../components/containers/Index';
+
+// Always start navigation at the top of the page
+const ScrollToTop = () => {
+    window.scrollTo(0, 0);
+
+    return null;
+};
 
 class Base extends React.Component {
 
     render() {
         return (
             <div>
-                Hello world!
+                <Router>
+                    <div>
+                        <Route component={ScrollToTop} />
+                        <Switch>
+                            <Route exact path="/" component={Index} />
+                        </Switch>
+                    </div>
+                </Router>
             </div>
         );
     }
