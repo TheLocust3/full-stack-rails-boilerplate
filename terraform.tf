77c9b808878de0882fe3ddde8d93c1def050a8f9
TheLocust3 <jake.kinsella@gmail.com>
2018-09-17 21:35:07 -0400

    Update terraform infastructure

--- terraform.tf
+++ terraform.tf
@@ -10,9 +10,33 @@ variable "username" {
   default = "testusername"
 }
 
+variable "key" {
+  default = "test"
+}
+
 data "aws_ami" "image" {
-  name_regex = "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server"
+  name_regex  = "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server"
   most_recent = true
+}
+
+data "aws_availability_zone" "zone" {
+  name = "us-east-1a"
+}
+
+data "aws_subnet" "east1" {
+  availability_zone = "us-east-1a"
+}
+
+data "aws_subnet" "east2" {
+  availability_zone = "us-east-1b"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_acm_certificate" "certificate" {
+  domain = "www.jakekinsella.com"
 }
 
 resource "random_id" "database_password" {
@@ -23,100 +47,98 @@ resource "random_id" "database_password" {
   byte_length = 16
 }
 
-resource "random_id" "random" { // Using this for snapshot names
+resource "random_id" "random" {
   keepers = {
-    password = "${var.name}"
+    password = "${var.name}" // Using this for snapshot names
   }
 
   byte_length = 8
 }
 
 resource "aws_security_group" "security_group" {
-  name = "${var.name} security group"
+  name        = "${var.name} security group"
   description = "Managed by Terraform"
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_security_group" "db_security_group" {
-  name = "${var.name} rds security group"
+  name        = "${var.name} rds security group"
   description = "Managed by Terraform"
 
   ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
     security_groups = ["${aws_security_group.security_group.id}"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 
 data "aws_iam_policy_document" "ec2-role" {
   statement {
     actions = ["sts:AssumeRole"]
 
     principals {
-      type = "Service"
+      type        = "Service"
       identifiers = ["ec2.amazonaws.com"]
     }
   }
 }
 
 resource "aws_iam_role" "ec2-role" {
-  name = "${var.name}-ec2"
+  name               = "${var.name}-ec2"
   assume_role_policy = "${data.aws_iam_policy_document.ec2-role.json}"
 }
 
 resource "aws_iam_role_policy_attachment" "ec2-role-for-codedeploy" {
-  role = "${aws_iam_role.ec2-role.name}"
+  role       = "${aws_iam_role.ec2-role.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy"
 }
 
 resource "aws_iam_instance_profile" "ec2-profile" {
-  name  = "${var.name}_profile"
+  name = "${var.name}_profile"
   role = "${aws_iam_role.ec2-role.name}"
 }
-
 
 data "aws_iam_policy_document" "codedeploy" {
   statement {
     actions = ["sts:AssumeRole"]
 
     principals {
-      type = "Service"
+      type        = "Service"
       identifiers = ["codedeploy.amazonaws.com"]
     }
   }
@@ -129,36 +151,97 @@ resource "aws_iam_role" "deployment" {
 }
 
 resource "aws_iam_role_policy_attachment" "deployment" {
-  role = "${aws_iam_role.deployment.name}"
+  role       = "${aws_iam_role.deployment.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
 }
 
-
-resource "aws_instance" "instance" {
-  ami = "${data.aws_ami.image.id}"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.security_group.id}"]
-  associate_public_ip_address = true
+resource "aws_launch_configuration" "launch" {
+  name                 = "${var.name}-config"
+  image_id             = "${data.aws_ami.image.id}"
+  instance_type        = "t2.micro"
+  security_groups      = ["${aws_security_group.security_group.id}"]
   iam_instance_profile = "${aws_iam_instance_profile.ec2-profile.name}"
-  key_name = "${var.name}"
+  key_name             = "${var.key}"
+  enable_monitoring    = false
 
-  tags {
-    Name = "${var.name}"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lb" "load_balancer" {
+  name               = "${var.name}-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.security_group.id}"]
+  subnets            = ["${data.aws_subnet.east1.id}", "${data.aws_subnet.east2.id}"]
+}
+
+resource "aws_lb_target_group" "target" {
+  name     = "${var.name}-targets"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${data.aws_vpc.default.id}"
+}
+
+resource "aws_lb_listener" "lb_listener1" {
+  load_balancer_arn = "${aws_lb.load_balancer.arn}"
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "${data.aws_acm_certificate.certificate.arn}"
+
+  default_action {
+    target_group_arn = "${aws_lb_target_group.target.arn}"
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_listener" "lb_listener2" {
+  load_balancer_arn = "${aws_lb.load_balancer.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_lb_target_group.target.arn}"
+    type             = "forward"
+  }
+}
+
+resource "aws_autoscaling_group" "autoscaling" {
+  name             = "${var.name}"
+  force_delete     = true
+  max_size         = "2"
+  min_size         = "1"
+  desired_capacity = "1"
+
+  launch_configuration = "${aws_launch_configuration.launch.name}"
+  availability_zones   = ["${data.aws_availability_zone.zone.name}"]
+  target_group_arns    = ["${aws_lb_target_group.target.arn}"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "${var.name}"
+    propagate_at_launch = true
   }
 }
 
 resource "aws_db_instance" "db_instance" {
-  identifier = "${var.name}"
-  allocated_storage = "5"
-  storage_type = "gp2"
-  engine = "postgres"
-  engine_version = "9.6.3"
-  instance_class = "db.t2.micro"
+  identifier             = "${var.name}"
+  allocated_storage      = "5"
+  storage_type           = "gp2"
+  engine                 = "postgres"
+  engine_version         = "9.6.6"
+  instance_class         = "db.t2.micro"
   vpc_security_group_ids = ["${aws_security_group.db_security_group.id}"]
 
   final_snapshot_identifier = "${var.name}-final-snapshot-${random_id.database_password.hex}"
 
-  name = "${var.name}"
+  name     = "${var.name}"
   username = "${var.username}"
   password = "${random_id.database_password.hex}"
 }
@@ -168,19 +251,51 @@ resource "aws_codedeploy_app" "application" {
 }
 
 resource "aws_codedeploy_deployment_group" "deployment" {
-  app_name = "${aws_codedeploy_app.application.name}"
+  app_name              = "${aws_codedeploy_app.application.name}"
   deployment_group_name = "production"
-  service_role_arn = "${aws_iam_role.deployment.arn}"
+  service_role_arn      = "${aws_iam_role.deployment.arn}"
+  autoscaling_groups    = ["${aws_autoscaling_group.autoscaling.id}"]
 
-  ec2_tag_filter {
-    type = "KEY_AND_VALUE"
-    key = "Name"
-    value = "${var.name}"
+  deployment_style {
+    deployment_option = "WITHOUT_TRAFFIC_CONTROL"
+    deployment_type   = "IN_PLACE"
   }
+
+  lifecycle {
+    ignore_changes = ["*"]
+  }
+
+  // Terraform fails to setup Blue Green deployment so currently just set it up in AWS console
+  /*deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
+  }
+
+  load_balancer_info {
+    target_group_info {
+      name = "${aws_lb_target_group.target.name}"
+    }
+  }
+
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout    = "STOP_DEPLOYMENT"
+      wait_time_in_minutes = 60
+    }
+
+    green_fleet_provisioning_option {
+      action = "COPY_AUTO_SCALING_GROUP"
+    }
+
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 10
+    }
+  }*/
 }
 
-output "instance_ip" {
-  value = "${aws_instance.instance.public_ip}"
+output "dns_name" {
+  value = "${aws_lb.load_balancer.dns_name}"
 }
 
 output "database_username" {
